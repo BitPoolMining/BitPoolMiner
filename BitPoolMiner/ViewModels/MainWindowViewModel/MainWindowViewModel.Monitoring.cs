5b0ef0a028a4e9c23400a7aef875cc7ef67452eb
saltyminer <34531153+saltyminer@users.noreply.github.com>
2018-06-14 00:23:38 -0500

    Refactored MainWindowViewModel

---
+++ BitPoolMiner/ViewModels/MainWindowViewModel/MainWindowViewModel.Monitoring.cs
@@ -0,0 +1,117 @@
+﻿using BitPoolMiner.Enums;
+using BitPoolMiner.Models;
+using BitPoolMiner.Persistence.API;
+using BitPoolMiner.Utils;
+using BitPoolMiner.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace BitPoolMiner.ViewModels
+{
+    /// <summary>
+    /// Handles all monitoring and UI related tasks for the Miner and GPU monitoring statistics
+    /// </summary>
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        #region Init
+
+        /// <summary>
+        /// Init timer used for monitoring and mining stats
+        /// </summary>
+        private void InitMonitoringCheckTimer()
+        {
+            MinerStatusCheckTimer = new DispatcherTimer();
+            MinerStatusCheckTimer.Tick += MinerStatusCheckTimer_Elapsed;
+            MinerStatusCheckTimer.Interval = TimeSpan.FromSeconds(3);  // 30 second default right now.  EWBF won't display any data until it submits the first share.
+            MinerStatusCheckTimer.Start();
+        }
+
+        #endregion
+
+        #region Properties
+
+        // Timer for Monitoring Miner
+        private DispatcherTimer MinerStatusCheckTimer;
+
+        // Miner monitor stats grouped by coin 
+        private ObservableCollection<MinerMonitorStat> minerMonitorStatListGrouped;
+        public ObservableCollection<MinerMonitorStat> MinerMonitorStatListGrouped
+        {
+            get
+            {
+                return minerMonitorStatListGrouped;
+            }
+            set
+            {
+                minerMonitorStatListGrouped = value;
+                OnPropertyChanged("MinerMonitorStatListGrouped");
+            }
+        }
+
+        #endregion
+
+        #region Monitoring
+
+        /// <summary>
+        /// Monitoring timer tick event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void MinerStatusCheckTimer_Elapsed(object sender, EventArgs e)
+        {
+            // Call miner RPC and post results to API
+            GetMinerMonitoringResults();
+        }
+
+        /// <summary>
+        /// Load list of miner monitor stats
+        /// </summary>
+        public void GetMinerMonitoringResults()
+        {
+            try
+            {
+                if (Application.Current.Properties["AccountID"] != null)
+                {
+                    // Load list of miner monitor stats
+                    List<MinerMonitorStat> minerMonitorStatList = new List<MinerMonitorStat>();
+                    MinerMonitorStatsAPI minerMonitorStatsAPI = new MinerMonitorStatsAPI();
+                    minerMonitorStatList = minerMonitorStatsAPI.GetMinerMonitorStats().ToList();
+
+                    // Populate properties for UI binding
+                    // Group stats by coin to show details
+                    List<MinerMonitorStat> minerMonitorStatListGrouped = minerMonitorStatList
+                        .Where(x => x.CoinType != CoinType.UNDEFINED)
+                        .GroupBy(l => l.CoinType)
+                        .Select(cl => new MinerMonitorStat
+                        {
+                            CoinLogo = cl.First().CoinLogo,
+                            CoinType = cl.First().CoinType,
+                            CountStats = cl.Count(),
+                            HashRate = cl.Sum(c => c.HashRate)
+                        }).ToList();
+
+                    // Format the hashrate of each grouped sum of hashrate per cointype
+                    foreach (MinerMonitorStat minerMonitorStat in minerMonitorStatListGrouped)
+                    {
+                        minerMonitorStat.DisplayHashRate = HashrateFormatter.Format(minerMonitorStat.CoinType, minerMonitorStat.HashRate);
+                    }
+
+                    MinerMonitorStatListGrouped = new ObservableCollection<MinerMonitorStat>(minerMonitorStatListGrouped);
+
+                    // Notify UI of change
+                    OnPropertyChanged("MinerMonitorStatListGrouped");
+                }
+            }
+            catch (Exception e)
+            {
+                ShowError(string.Format("Error loading monitor data"));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ BitPoolMiner/ViewModels/MainWindowViewModel/MainWindowViewModel.cs
@@ -0,0 +1,127 @@
+﻿using BitPoolMiner.Enums;
+using BitPoolMiner.Miners;
+using BitPoolMiner.Models;
+using BitPoolMiner.Persistence.API;
+using BitPoolMiner.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+
+namespace BitPoolMiner.ViewModels
+{
+    /// <summary>
+    /// View model for main window
+    /// Monitoring logic moved to partial class
+    /// </summary>
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        #region Init
+
+        public MainWindowViewModel()
+        {
+            // Wire up commands for mining
+            CommandStartMining = new RelayCommand(StartAllMiners);
+            CommandStopMining = new RelayCommand(StopAllMiners);
+
+            // Initialize objects
+            InitMining();
+            InitMonitoringCheckTimer();
+
+            // Init account worker list
+            AccountWorkersList = new ObservableCollection<AccountWorkers>();
+
+            // Get a reference to the MainView window
+            mainWindow = (MainWindow)Application.Current.MainWindow;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public MainWindow mainWindow;
+
+        // Main Title string used to bind to UI
+        private string labelMainTitle;
+        public string LabelMainTitle
+        {
+            get
+            {
+                if (Application.Current.Properties["WorkerName"] != null)
+                    return string.Format("BITPOOL MINER - {0}", Application.Current.Properties["WorkerName"].ToString());
+                else
+                    return "BITPOOL MINER - OFFLINE";
+            }
+            set
+            {
+                if (labelMainTitle == value)
+                    return;
+
+                if (Application.Current.Properties["WorkerName"] != null)
+                    labelMainTitle = string.Format("BITPOOL MINER - {0}", Application.Current.Properties["WorkerName"].ToString());
+                else
+                    labelMainTitle = "BITPOOL MINER - OFFLINE";
+
+                // Set title on main window
+                if (mainWindow.LabelMainTitle != null)
+                    mainWindow.LabelMainTitle.Content = labelMainTitle;
+
+                // Change value and notify UI
+                OnPropertyChanged();
+            }
+        }
+
+        // Account workers property to bind to UI
+        private ObservableCollection<AccountWorkers> accountWorkersList;
+        public ObservableCollection<AccountWorkers> AccountWorkersList
+        {
+            get
+            {
+                InitAccountWorkerList();
+                return accountWorkersList;
+            }
+            set
+            {
+                accountWorkersList = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        #endregion
+
+        #region Main Window Menu Worker List Methods
+
+        private void InitAccountWorkerList()
+        {
+            // If there is an account id set, and there are no records in the list then load from API
+            if (Application.Current.Properties["AccountID"] != null && accountWorkersList.Count == 0)
+            {
+                // Load list of account workers
+                AccountWorkersAPI accountWorkersAPI = new AccountWorkersAPI();
+                accountWorkersList = accountWorkersAPI.GetAccountWorkers();
+            }
+        }
+
+        public void AddAccountWorker(Guid AccountGuid, string WorkerName)
+        {
+
+            AccountWorkers accountWorkers = new AccountWorkers();
+            accountWorkers.AccountGuid = AccountGuid;
+            accountWorkers.WorkerName = WorkerName;
+            AccountWorkersList.Add(accountWorkers);
+            // Notify UI of change
+            OnPropertyChanged("AccountWorkersList");
+        }
+
+        public void RemoveAccountWorker(AccountWorkers accountWorkers)
+        {
+            AccountWorkersList.Remove(accountWorkers);
+            // Notify UI of change
+            OnPropertyChanged("AccountWorkersList");
+        }
+
+        #endregion
+    }
+}
